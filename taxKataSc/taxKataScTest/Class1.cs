276c3d2d315248545553e35e04e278ee6c8fd174
(U118538) <sylvain.vivenot@swisscaution.ch>
2019-05-08 11:01:06 +0200

    2e kata

--- taxKataSc/taxKataScTest/Class1.cs
+++ taxKataSc/taxKataScTest/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 using NFluent;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -11,7 +12,12 @@ namespace taxKataScTest
         public void Should_tax_be_165_when_weight_is_under_3500()
         {
             TaxCalculator taxCalculator = new TaxCalculator();
-            Decimal tax = taxCalculator.Calculate(10);
+            Vehicule myVan = new Vehicule()
+            {
+                Type = VehiculeTypes.Van,     
+                Weight = 10,
+            };
+            Decimal tax = taxCalculator.Calculate(myVan);
             Check.That<Decimal>(tax).IsEqualTo(165);
         }
 
@@ -19,7 +25,12 @@ namespace taxKataScTest
         public void Should_tax_be_190_when_weight_is_over_3500()
         {
             TaxCalculator taxCalculator = new TaxCalculator();
-            Decimal tax = taxCalculator.Calculate(10000);
+            Vehicule myVan = new Vehicule()
+            {
+                Type = VehiculeTypes.Van,
+                Weight = 10000,
+            };
+            Decimal tax = taxCalculator.Calculate(myVan);
             Check.That<Decimal>(tax).IsEqualTo(190);
         }
 
@@ -27,28 +38,84 @@ namespace taxKataScTest
         public void Should_tax_be_190_when_weight_is_equal_3500()
         {
             TaxCalculator taxCalculator = new TaxCalculator();
-            Decimal tax = taxCalculator.Calculate(3500);
+            Vehicule myVan = new Vehicule()
+            {
+                Type = VehiculeTypes.Van,
+                Weight = 3500,
+            };
+            Decimal tax = taxCalculator.Calculate(myVan);
             Check.That<Decimal>(tax).IsEqualTo(190);
         }
 
+        [Test]
+        public void Should_return_110_when_car_older_than_1_march2001_and_engine_less_equal_1550cc()
+        {
+            TaxCalculator taxCalculator = new TaxCalculator();
+            Vehicule myCar = new Vehicule()
+            {
+                Type = VehiculeTypes.Car,
+                ImmatDate = new DateTime(2000, 1, 1),
+                EngineCc = 1500
+            };
+            Decimal tax = taxCalculator.Calculate(myCar);
+            Check.That<Decimal>(tax).IsEqualTo(110);
+        }
+
+        [Test]
+        public void Should_return_165_when_car_older_than_1_march2001_and_engine_more_than_1550cc()
+        {
+            TaxCalculator taxCalculator = new TaxCalculator();
+            Vehicule myCar = new Vehicule()
+            {
+                Type = VehiculeTypes.Car,
+                ImmatDate = new DateTime(2001, 1, 1),
+                EngineCc = 1600
+            };
+            Decimal tax = taxCalculator.Calculate(myCar);
+            Check.That<Decimal>(tax).IsEqualTo(165);
+        }
 
     }
 
     public class Vehicule
     {
+        public VehiculeTypes Type { get; internal set; }
+        public DateTime ImmatDate { get; internal set; }
+        public int EngineCc { get; internal set; }
+        public int Weight { get; internal set; }
+    }
 
+    public enum VehiculeTypes
+    {
+        Car = 0,
+        Van,
     }
 
     public class TaxCalculator
     {
-        public decimal Calculate(float weight)
+        public decimal Calculate(Vehicule vehicule)
         {
-            if (weight < 3500)
+            switch (vehicule.Type)
             {
-                return 165;
-            }
+                case VehiculeTypes.Car:
+                {
+                    if (vehicule.EngineCc > 1550 )
+                    {
+                        return 165;
+                    }
+                    return 110;
+                }
+                case VehiculeTypes.Van:
+                {
+                    if (vehicule.Weight < 3500)
+                    {
+                        return 165;
+                    }
 
-            return 190;
+                    return 190;
+                }
+            }
+            return 110.0m;
         }
     }
 }
